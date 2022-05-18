45917c612827210baacab8e8416e979a650395bc
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-05-18 15:21:48 +0530

    Add files via upload

---
+++ cart_service/src/main/java/com/cart/CartServiceApplication.java
@@ -0,0 +1,15 @@
+package com.cart;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class CartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartServiceApplication.class, args);
+	}
+
+}
