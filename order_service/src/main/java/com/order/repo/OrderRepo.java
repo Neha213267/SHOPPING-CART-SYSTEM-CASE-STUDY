9716f4f1ed6685878bc61abe33b32fb0ab0a8864
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-05-23 12:36:00 +0530

    Delete order_service directory

--- order_service/src/main/java/com/order/repo/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.order.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-import com.order.entity.OrderEntity;
-public interface OrderRepo extends MongoRepository<OrderEntity, Integer> {
-
-}
